899e1aadf697ba0db92be611b3ac091ab7360cec
Cedric Bahirwe <cedricaganzebahirwe@gmail.com>
2022-06-10 01:04:04 +0200

    Add Testing View

---
+++ Snotify/Snotify/TestingView.swift
@@ -0,0 +1,23 @@
+//
+//  TestingView.swift
+//  Snotify
+//
+//  Created by Cédric Bahirwe on 09/06/2022.
+//
+
+import SwiftUI
+import CoreLocation
+
+struct TestingView: View {
+    var body: some View {
+        VStack {
+        }
+    }
+
+}
+
+struct TestingView_Previews: PreviewProvider {
+    static var previews: some View {
+        TestingView()
+    }
+}
