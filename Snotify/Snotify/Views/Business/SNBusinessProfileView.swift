23e2271abfa807b8c4b4215d5b6dc7d5921718d9
Cedric Bahirwe <cedricaganzebahirwe@gmail.com>
2022-07-14 21:28:35 +0200

    Add Sign out flow

--- Snotify/Snotify/Views/Business/SNBusinessProfileView.swift
+++ Snotify/Snotify/Views/Business/SNBusinessProfileView.swift
@@ -6,13 +6,13 @@
 //
 
 import SwiftUI
-
+import MapKit
 
 struct SNBusinessProfileView: ProfileViewProtocol {
     var sessionType: SNSessionType = .customer
-
+    @State private var showSettingsView = false
     var body: some View {
-        VStack {
+        ZStack {
             VStack(alignment: .leading) {
                 HStack(spacing: 20) {
                     Image("iphone1")
@@ -49,11 +49,21 @@ struct SNBusinessProfileView: ProfileViewProtocol {
             }
             .padding()
 
-            Spacer()
-
-
         }
         .navigationBarTitleDisplayMode(.inline)
+        .confirmationDialog(
+            "Settings",
+            isPresented: $showSettingsView
+        ) {
+            Button {
+                // Handle import action.
+            } label: {
+                Text("Learn More")
+            }
+            Button("Sign Out", role: .destructive) {
+                SNFirebaseManager.shared.signOut()
+            }
+        }
         .toolbar {
             ToolbarItem(placement: .navigationBarLeading) {
                 HStack {
@@ -70,7 +80,7 @@ struct SNBusinessProfileView: ProfileViewProtocol {
                 }
 
                 Button {
-                    //
+                    showSettingsView.toggle()
                 } label: {
                     Label("paramètres", systemImage: "gear")
                 }
@@ -137,10 +147,7 @@ private extension SNBusinessProfileView {
                     .overlay(ctaOverlay)
             }
 
-
-            Button {
-                //
-            } label: {
+            Button(action: openMapForPlace) {
                 Text("Voir sur Maps")
                     .font(.callout.weight(.semibold))
                     .padding(.horizontal, 8)
@@ -172,6 +179,22 @@ private extension SNBusinessProfileView {
             .stroke(Color.secondary, lineWidth: 1)
     }
 
+    func openMapForPlace() {
+        let latitude:CLLocationDegrees = 0
+        let longitude:CLLocationDegrees = 0
+
+        let regionDistance:CLLocationDistance = 10000
+        let coordinates = CLLocationCoordinate2DMake(latitude, longitude)
+        let regionSpan = MKCoordinateRegion(center: coordinates, latitudinalMeters: regionDistance, longitudinalMeters: regionDistance)
+        let options = [
+            MKLaunchOptionsMapCenterKey: NSValue(mkCoordinate: regionSpan.center),
+            MKLaunchOptionsMapSpanKey: NSValue(mkCoordinateSpan: regionSpan.span)
+        ]
+        let placemark = MKPlacemark(coordinate: coordinates, addressDictionary: nil)
+        let mapItem = MKMapItem(placemark: placemark)
+        mapItem.name = "Restaurant Name"
+        mapItem.openInMaps(launchOptions: options)
+    }
 }
 struct SNBusinessProfileView_Previews: PreviewProvider {
     static var previews: some View {
